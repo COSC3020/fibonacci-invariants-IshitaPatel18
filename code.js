2cd829899abe714fcdbc7bdd3b4d00d075179c93
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-07 15:38:32 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
-function fib(n) {
-    return n;
+function fib(value)
+{
+    let tmp = [];
+    tmp = one(value, tmp);
+    return tmp;
+}
+
+function sum(value, tmp)
+{
+    
+    if(value <= 1)
+    {
+        tmp[value] = value;
+        return value;
+    }
+    
+   // tmp[value] = one(value -1, tmp) + one(value -2, tmp);
+    
+    return tmp[value] = one(value - 1, tmp) + one(value -2, tmp);
 }
