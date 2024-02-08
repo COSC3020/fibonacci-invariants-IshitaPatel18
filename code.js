866d927b35c3c10958b920121a74747d472bc0ab
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-07 17:31:27 -0700

    Update code.js

--- code.js
+++ code.js
@@ -12,20 +12,19 @@ function sum(value, tmp)
         tmp[0] = 0;
         return 0;
     }
-    else if(value === 0)
+    if(value === 0)
     {
         tmp[0] = 0;
         return 0;
     }
-    else if(value === 1)
+    if(value === 1)
     {
         tmp[0] = 0;
         tmp[1] = 1;
         return 1;
     }
-    else
-    {
-        tmp[value] = sum(value -1, tmp) + sum(value -2, tmp);
-        return tmp[value];
-    }
+    
+    tmp[value] = sum(value -1, tmp) + sum(value -2, tmp);
+    return tmp[value];
+
 }
